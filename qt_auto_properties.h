3b268026a2ec4609384e999428f4598cca467e00
Wes Hardaker <opensource@hardakers.net>
2012-08-03 12:29:14 -0700

    renamed everything to include qt in front

--- qt_auto_properties.h
+++ qt_auto_properties.h
@@ -1,14 +1,14 @@
-#ifndef AUTO_GETSETTOOLS_H
-#define AUTO_GETSETTOOLS_H
+#ifndef QTAUTO_PARAMETERS_H
+#define QTAUTO_PARAMETERS_H
 
-#ifndef _AUTO_OPERATE
+#ifndef _QTAUTO_OPERATE
 
 /* replacement macros so you don't have to comment them out */
 
-#define AUTO_GET(autotype, lcname)
-#define AUTO_GET_SIGNAL(autotype, lcname)
-#define AUTO_GET_SET_SIGNAL(autotype, lcname)
-#define AUTO_HERE
+#define QTAUTO_GET(autotype, lcname)
+#define QTAUTO_GET_SIGNAL(autotype, lcname)
+#define QTAUTO_GET_SET_SIGNAL(autotype, lcname)
+#define QTAUTO_HERE
 
 #else 
 
@@ -16,37 +16,37 @@
  * will properly terminate every experssion, allowing for more readable code.
  * IE: this deliberately doesn't have a trailing ';'
  */
-#define _AUTO_TYPE(autotype, lcname)            \
+#define _QTAUTO_TYPE(autotype, lcname)            \
     private:                                                \
          autotype m_ ## lcname
                                                             
 
-#define _AUTO_GET(autotype, lcname)         \
+#define _QTAUTO_GET(autotype, lcname)         \
     public:                                                 \
          const autotype &lcname() const {                   \
               return m_ ## lcname;                          \
          }                                                  \
 
-#define _AUTO_SIGNAL(autotype, lcname)                      \
+#define _QTAUTO_SIGNAL(autotype, lcname)                      \
     signals:                                                \
         void lcname ## Changed();                           \
 
-#define AUTO_GET(autotype, lcname)                          \
-    Q_PROPERTY(autotype lcname READ lcname);                \
-    _AUTO_GET(autotype, lcname)                             \
-    _AUTO_TYPE(autotype, lcname)
+#define QTAUTO_GET(autotype, lcname)                          \
+    Q_PROPERTY(autotype lcname READ lcname)                \
+    _QTAUTO_GET(autotype, lcname)                             \
+    _QTAUTO_TYPE(autotype, lcname)
 
-#define AUTO_GET_SIGNAL(autotype, lcname)                        \
-    Q_PROPERTY(autotype lcname READ lcname NOTIFY lcname ## Changed); \
-    _AUTO_GET(autotype, lcname)                                       \
-    _AUTO_SIGNAL(autotype, lcname)                                    \
-    _AUTO_TYPE(autotype, lcname)
+#define QTAUTO_GET_SIGNAL(autotype, lcname)                        \
+    Q_PROPERTY(autotype lcname READ lcname NOTIFY lcname ## Changed) \
+    _QTAUTO_GET(autotype, lcname)                                       \
+    _QTAUTO_SIGNAL(autotype, lcname)                                    \
+    _QTAUTO_TYPE(autotype, lcname)
 
 
-#define AUTO_GET_SET_SIGNAL(autotype, lcname, ucname)                   \
-    Q_PROPERTY(autotype lcname READ lcname WRITE set ## ucname NOTIFY lcname ## Changed);                \
-    _AUTO_GET(autotype, lcname)                                         \
-    _AUTO_SIGNAL(autotype, lcname)                                      \
+#define QTAUTO_GET_SET_SIGNAL(autotype, lcname, ucname)                   \
+    Q_PROPERTY(autotype lcname READ lcname WRITE set ## ucname NOTIFY lcname ## Changed)                \
+    _QTAUTO_GET(autotype, lcname)                                         \
+    _QTAUTO_SIGNAL(autotype, lcname)                                      \
                                                                         \
     public slots:                                                       \
     void set ## ucname(const autotype &newval) {                        \
@@ -56,7 +56,7 @@
         }                                                               \
     }                                                                   \
                                                                         \
-    _AUTO_TYPE(autotype, lcname)
+    _QTAUTO_TYPE(autotype, lcname)
 
-#endif /* _AUTO_OPERATE */
-#endif /* AUTO_GETSETTOOLS_H */
+#endif /* _QTAUTO_OPERATE */
+#endif /* QTAUTO_PARAMETERS_H */
--- test.h
+++ test.h
@@ -1,4 +1,6 @@
-    AUTO_GET_SET_SIGNAL(int, myIntProperty);
-//
-AUTO_HERE
-/* AGST */ Q_PROPERTY(int myIntProperty READ myIntProperty WRITE setMyIntProperty NOTIFY myIntPropertyChanged); public: const int &myIntProperty() const { return m_myIntProperty; } signals: void myIntPropertyChanged(); public slots: void setMyIntProperty(const int &newval) { if (newval != m_myIntProperty) { m_myIntProperty = newval; emit myIntPropertyChanged(); } } private: int m_myIntProperty
+    QTAUTO_GET_SET_SIGNAL(int, myIntProperty);
+
+foo;
+
+// QTAUTO_HERE
+    /* AGST */ Q_PROPERTY(int myIntProperty READ myIntProperty WRITE setMyIntProperty NOTIFY myIntPropertyChanged) public: const int &myIntProperty() const { return m_myIntProperty; } signals: void myIntPropertyChanged(); public slots: void setMyIntProperty(const int &newval) { if (newval != m_myIntProperty) { m_myIntProperty = newval; emit myIntPropertyChanged(); } } private: int m_myIntProperty;
